46f632c1a49b9f6fdb792371851211649cb41fa9
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-05-17 18:42:26 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+     constructor(props) {
+          super(props);
+          this.state = { 
+               timesClicked: 0
+           }
+     }
+
+     handleClick = () => {
+          this.setState(prevState => {
+               return { timesClicked: prevState.timesClicked + 1 };
+          });
+     }
+
+     render() { 
+          return ( 
+               <div>
+                    Time Snatcher 
+                    <button style={{ marginLeft: 10 + 'px'}} onClick={this.handleClick}>{this.state.timesClicked}</button>
+               </div>
+           );
+     }
+}
+ 
+export default DigitalClicker;
